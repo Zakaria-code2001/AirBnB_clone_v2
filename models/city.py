db1d4a16b51fdeebf1f4e7dbb5fb98ad7c13c8ac
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-18 21:41:14 +0100

    d

--- models/city.py
+++ models/city.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel
-
+from sqlalchemy import String, DateTime, Column, ForeignKey
+from sqlalchemy.orm import relationship
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
