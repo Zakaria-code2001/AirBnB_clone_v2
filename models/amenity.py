b1bde8a2478abc9b033bdb2a79af78c3ef7260b8
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-19 01:30:32 +0100

    dd

--- models/amenity.py
+++ models/amenity.py
@@ -1,18 +1,15 @@
-from sqlalchemy import Table, Column, String, ForeignKey
+#!/usr/bin/python3
+""" Amenity Module for HBNB project """
+import os
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+
 from models.base_model import BaseModel, Base
-from os import getenv
+
 
 class Amenity(BaseModel, Base):
-    """
-    Amenity inherits from BaseModel and Base
-    """
+    """Represents an amenity"""
     __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenity = Table('place_amenity', Base.metadata,
-                              Column('amenity_id', String(60), ForeignKey('amenities.id'), primary_key=True),
-                              Column('place_id', String(60), ForeignKey('places.id'), primary_key=True))
-        
-        places = relationship('Place', secondary='place_amenity', back_populates='amenities')
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
