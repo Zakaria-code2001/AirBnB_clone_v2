2e8b60aa88dccc05b792ef2a68494d6a1ee37cb2
Zakaria-code2001 <zakariamohammadi91@gmail.com>
2024-03-20 21:07:09 +0100

    fdgs

--- models/amenity.py
+++ models/amenity.py
@@ -11,8 +11,9 @@ from sqlalchemy.orm import relationship
 class Amenity(BaseModel, Base):
     """Representation of Amenity """
     __tablename__ = 'amenities'
+    name = Column(String(128), nullable=False)
     if getenv("HBNB_TYPE_STORAGE") == 'db':    
-        name = Column(String(128), nullable=False)
+        place_amenities = relationship('Place',secondary='place_amenity',back_populates='amenities')
     else:
         name = ""
 
